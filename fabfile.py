452e60ca742f3bf88d87a7d410817f324b7bdacb
Samuel Fuentes <info@samuelfuentes.com>
2014-07-05 22:09:08 +0200

    Add first deploy

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,26 @@
+import os
+
 from fabric.api import *
 
 env.hosts = ['ubuntu@10.211.55.6']
 
+path, project_name = os.path.split(os.getcwd())
+git_repo_remote = 'https://github.com/samufuentes/django_fabric_example.git'
+
+@task
+def prepare_dependancies():
+    # To avoid password prompt manually add the following to the
+    # /etc/sudoers. Use sudo visudo to edit it
+    # ubuntu ALL=(ALL) NOPASSWD: ALL
+    sudo("apt-get -y install git-core", shell=False) # shell=False avoids pwd prompt. -y avoids confirmation prompt
+
+@task
+def first_deploy():
+    prepare_dependancies()
+    # Avoid rsa prompt
+    run('echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config')
+    run("git clone %s" %git_repo_remote)
+
 @task
 def deploy_staging():
-    run('touch test.tst')
+    pass
