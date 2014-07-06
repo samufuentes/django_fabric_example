ecaeaf4f0944fb59ebcb9139f79af1e06a8aabff
Samuel Fuentes <info@samuelfuentes.com>
2014-07-06 13:35:25 +0200

    Add nginx and uwsgi

--- fabfile.py
+++ fabfile.py
@@ -8,14 +8,17 @@ path, project_name = os.path.split(os.getcwd())
 git_repo_remote = 'https://github.com/samufuentes/django_fabric_example.git'
 
 @task
-def prepare_dependancies():
+def install_dependancies():
     # To avoid password prompt manually add the following to the
     # /etc/sudoers. Use sudo visudo to edit it
     # ubuntu ALL=(ALL) NOPASSWD: ALL
 
-    # shell=False avoids pwd prompt. -y avoids confirmation prompt
-    sudo("apt-get -y install git-core python-setuptools", shell=False)
-    sudo("easy_install pip", shell=False)
+    sudo("apt-get -y install build-essential")
+    sudo("apt-get -y install python-dev")
+    sudo("apt-get -y install git-core")
+    sudo("apt-get -y install python-setuptools")
+    sudo("apt-get -y install nginx")
+    sudo("easy_install pip")
     sudo("pip install virtualenv")
 
 @task
@@ -29,6 +32,19 @@ def install_requirements():
         run("env/bin/pip install -r requirements.txt")
 
 @task
+def configure_server():
+    with cd(project_name):
+        sudo("cp production_files/uwsgi.conf /etc/init/uwsgi.conf")
+        sudo("cp production_files/nginx_example /etc/nginx/sites-available")
+        sudo("ln -s /etc/nginx/sites-available/nginx_example /etc/nginx/sites-enabled/nginx_example")
+
+@task
+def restart_services():
+    run("service nginx restart")
+    run("service uwsgi restart")
+
+
+@task
 def first_deploy():
     prepare_dependancies()
     # Avoid rsa prompt
@@ -36,9 +52,12 @@ def first_deploy():
     run("git clone %s" %git_repo_remote)
     create_env()
     install_requirements()
+    configure_server()
+    restart_services()
 
 @task
 def deploy():
     with cd(project_name):
         run("git pull origin master")
     install_requirements()
+    restart_services()
