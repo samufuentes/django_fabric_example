195bcae19629be17aa56644cca1fb7cd74dd9805
Samuel Fuentes <info@samuelfuentes.com>
2014-07-14 03:29:01 +0200

    Add check for auto deploy branch to be master

--- auto_deploy/views.py
+++ auto_deploy/views.py
@@ -1,17 +1,27 @@
+import json
+
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 from fabfile import auto_deploy
 
+@csrf_exempt
 def home(request):
-    deployed = False
-    try:
-        auto_deploy()
-        deployed = True
-    except:
-        e = sys.exc_info()
-        print e
-    finally:
-        # This line will only get executed if there's an error,
-        # since otherwise the server will get restarted
-        # TODO: make auto_deploy async
-        return HttpResponse('Deployed: %s' %deployed)
+    if request.method == 'POST':
+        payload = json.loads(request.body)
+        if payload['ref'] == 'refs/heads/master':
+            deployed = False
+            try:
+                auto_deploy()
+                deployed = True
+            except:
+                e = sys.exc_info()
+                print e
+            finally:
+                # This line will only get executed if there's an error,
+                # since otherwise the server will get restarted
+                # TODO: make auto_deploy async
+                return HttpResponse('Deployed: %s' %deployed)
+        return HttpResponse('Not deployed')
+    return HttpResponse('Not deployed')
